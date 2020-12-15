94ce117a3e245c8e1b1b908a9fb7ae396bfd15a0
2arian3 <aryan.boukani2@gmail.com>
2020-12-15 12:52:55 +0330

    implemented comparisons for cards, columns and states

--- First Project/P1.py
+++ First Project/P1.py
@@ -1,7 +1,7 @@
 import re
+import copy
 from enum import Enum
 
-columns = []
 cardComparisons = Enum('Comparison', [
     'SameColorBiggerNumber',
     'SameColorSmallerNumber',
@@ -18,6 +18,9 @@ class Card:
     def __repr__(self):
         return 'Number : ' + str(self.number) + '\n' + 'Color : ' + self.color
 
+    def __eq__(self, card):
+        return self.number == card.number and self.color == card.color
+
     def compare(self, card):
         if self.color == card.color:
             if self.number > card.number:
@@ -32,47 +35,55 @@ class Card:
 class Column :
 
     def __init__(self):
-        self.state = []
+        self.cards = []
 
     def __repr__(self):
-        return str(self.state)
+        return str(self.cards)
+    
+    def __eq__(self, column):
+        return len(self.cards) == len(column.cards) and all([self.cards[i] == column.cards[i] for i in range(len(self.cards))])
 
     def checkAvailability(self, card):
-        if self.state:
-            return self.state[-1].compare(card) == cardComparisons.SameColorBiggerNumber or self.state[-1].compare(card) == cardComparisons.DifferentColorBiggerNumber
+        if self.cards:
+            return self.cards[-1].compare(card) == cardComparisons.SameColorBiggerNumber or self.cards[-1].compare(card) == cardComparisons.DifferentColorBiggerNumber
         return True
 
     def putCardOnTop(self, card):
-        self.state.append(card)
+        self.cards.append(card)
     
     def removeCardFromTop(self):
-        return self.state.pop()
+        return self.cards.pop()
 
     def checkValidation(self):
-        return all([self.state[i].compare(self.state[i+1]) == cardComparisons.SameColorBiggerNumber for i in range(len(self.state)-1)])
+        return all([self.cards[i].compare(self.cards[i+1]) == cardComparisons.SameColorBiggerNumber for i in range(len(self.cards)-1)])
 
-def checkTermination():
-    return all([column.checkValidation() for column in columns])
+class State:
 
-columns.append(Column())
-columns[-1].putCardOnTop(Card(5, 'g'))
-columns[-1].putCardOnTop(Card(2, 'g'))
-columns[-1].putCardOnTop(Card(1, 'g'))
-columns[-1].putCardOnTop(Card(-1, 'g'))
-print(columns[-1].checkValidation())
+    def __init__(self, columns=[]):
+        self.columns = copy.deepcopy(columns)
+    
+    def __eq__(self, state):
+        return len(self.columns) == len(state.columns) and all([self.columns[i] == state.columns[i] for i in range(len(self.columns))])
+        
+def checkTermination(state):
+    return all([column.checkValidation() for column in state.columns])
 
-#Reading inputs from input.txt
-with open('input.txt', 'r') as src:
-    numberOfColumns, colors, numbers = map(int, src.readline().split(' '))
-    columns = [Column() for _ in range(numberOfColumns)]
-    lines = [src.readline().rstrip() for _ in range(numberOfColumns)]
-    for i in range(numberOfColumns):
-        if lines[i] is not '#':
-            for card in lines[i].split(' '):
-                number, color = map(str, re.split('(\d+)', card)[1:])
-                columns[i].putCardOnTop(Card(int(number), color))
-for i in range(numberOfColumns):
-    for j in range(numberOfColumns):
-        if i != j:
-            print(columns[j].checkAvailability(columns[i].state[-1]), end=' ')
-    print()
+def BreadthFirstSearch():
+    if checkTermination(): return True
+
+initialState = State()
+
+def main():
+    #Reading inputs from input.txt
+    with open('input.txt', 'r') as src:
+        numberOfColumns, colors, numbers = map(int, src.readline().split(' '))
+        initialState.columns = [Column() for _ in range(numberOfColumns)]
+        lines = [src.readline().rstrip() for _ in range(numberOfColumns)]
+        for i in range(numberOfColumns):
+            if lines[i] is not '#':
+                for card in lines[i].split(' '):
+                    number, color = map(str, re.split('(\d+)', card)[1:])
+                    initialState.columns[i].putCardOnTop(Card(int(number), color))
+
+if __name__ == '__main__':
+    main()
